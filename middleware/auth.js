2fa3b96bbfa0eb75387ff7b2e6d2be9874f19aa0
Muhammad Hamza <mh408800@gmail.com>
2023-11-20 12:03:37 +0500

    auth middleware updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,30 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const userModel = require("../models/userModel");
 
-const verifyToken = async (req,res,next)=>{
-    const token = req.body.token || req.query.token || req.headers["authorization"]
+const verifyToken = async (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["authorization"];
 
-    if (!token){
-        return res.status(400).json({ message: "token required!"})
+  if (!token) {
+    return res.status(400).json({ message: "token required!" });
+  }
+  try {
+    const decode = jwt.verify(token, process.env.secret_key);
+    const user = await userModel.findOne({ _id: decode._id });
+
+    if (user.isAdmin === "true") {
+      req.user = decode;
+      return next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: " access denied. don't have the privileges!" });
     }
-    try{
+  } catch (err) {
+    res.status(400).send({ message: "invalid token!" });
+  }
+  return next();
+};
 
-        const decode = jwt.verify(token, process.env.secret_key);
-        req.user = decode;
-        if (req.user && req.user.isAdmin === 'true'){
-            return next();
-        }
-        else {
-            return res.status(403).json({ message: " access denied. insufficient privileges!"})
-        }
-
-    }
-    catch(err){
-        res.status(400).send({message: "invalid token!"})
-    }
-    return next();
-}
-
-
-module.exports = verifyToken
+module.exports = verifyToken;
